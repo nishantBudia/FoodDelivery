7c229245196d44aeeef29f5118b11ce2c585d3c2
NishantBudia <nishantbudia@gmail.com>
2023-10-20 13:53:38 +0530

    Initial commit

---
+++ src/test/java/com/nishant/FoodDelivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishant.FoodDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
